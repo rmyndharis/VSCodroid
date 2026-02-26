af8a87934f9e068a7eb88aa9236a1c214d914baf
rmyndharis <yudhi@rmyndharis.com>
2026-02-26 16:06:19 +0700

    bump versionCode to 6, versionName to 0.2.4

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -29,8 +29,8 @@ android {
         applicationId = "com.vscodroid"
         minSdk = 33
         targetSdk = 36
-        versionCode = 5
-        versionName = "0.2.3"
+        versionCode = 6
+        versionName = "0.2.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
